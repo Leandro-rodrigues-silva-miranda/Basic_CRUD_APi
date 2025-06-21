a147470b0a5ab153eb1b9bd3f95c3900a6a3fbbb
unknown <leandrorsmlp@hotmail.com>
2025-06-21 12:40:23 -0300

    Primeiros testes de criação da API, Read criado

--- book_log/views.py
+++ book_log/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+from .models import Book,Category
+from .serializers import BookSerializer, CategorySerializer
+
+import json
+
+
+
+@api_view(['GET'])
+def get_books(request):
+
+    if request.method == 'GET':
+
+        books = Book.objects.all()
+
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
